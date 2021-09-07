46a722cb57b5cec12252d47b8797d987b6c2f78e
yoshi <julian404@icloud.com>
2021-09-08 00:21:46 +0200

    Fix: add IO_Mapper as testing dependency

--- src/io_mapper.c
+++ src/io_mapper.c
@@ -17,11 +17,29 @@ io_state_t IOMapper_GetForwardLightsState()
   return true;
 }
 
+// Umschalt-B
+io_state_t IOMapper_GetControlSource(void)
+{
+  return true;
+}
+
+// Richtung
+io_state_t IOMapper_GetDirectionState(void)
+{
+  return true;
+}
+
+
 void IOMapper_SetForwarLightState(io_state_t state)
 {
 }
 
 void IOMapper_SetBackwardLightState(io_state_t state)
+{
+
+}
+
+void IOMapper_SetDirectionAVR(io_state_t state)
 {
 
 }
