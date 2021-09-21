df8d4c9d8f676e537a5964ff67e3dc8a0f90c7ef
Julian Linn <julian404@icloud.com>
2021-09-21 22:00:38 +0000

    Bugfix/access actual pin values

--- src/gpio_driver.c
+++ src/gpio_driver.c
@@ -11,12 +11,27 @@
 //*****************************************************************************
 // Module Preprocessor Constants
 //*****************************************************************************
+#define INVALID_VALUE -1
+#define PIN_MAX 35
+
 enum Ports
 {
   PORTB,
   PORTC,
   PORTD,
   PORT_MAX
+};
+
+enum Pins
+{
+  PIN0,
+  PIN1,
+  PIN2,
+  PIN3,
+  PIN4,
+  PIN5,
+  PIN6,
+  PIN7,
 };
 
 //*****************************************************************************
@@ -31,27 +46,97 @@ enum Ports
 // Module Variable Definitions
 //*****************************************************************************
 volatile GPIO_Port_t *GPIO_Ports[PORT_MAX];
+static int8_t PinToPort[PIN_MAX] =
+    {
+        INVALID_VALUE, // 0
+        PORTD,         // 1
+        PORTD,         // 2
+        INVALID_VALUE, // 3
+        INVALID_VALUE, // 4
+        INVALID_VALUE, // 5
+        INVALID_VALUE, // 6
+        PORTB,         // 7
+        PORTB,         // 8
+        PORTD,         // 9
+        PORTD,         // 10
+        PORTD,         // 11
+        PORTB,         // 12
+        PORTB,         // 13
+        PORTB,         // 14
+        PORTB,         // 15
+        PORTB,         // 16
+        PORTB,         // 17
+        INVALID_VALUE, // 18
+        INVALID_VALUE, // 19
+        INVALID_VALUE, // 20
+        INVALID_VALUE, // 21
+        INVALID_VALUE, // 22
+        PORTC,         // 23
+        PORTC,         // 24
+        PORTC,         // 25
+        PORTC,         // 26
+        PORTC,         // 27
+        PORTC,         // 28
+        PORTC,         // 29
+        PORTD,         // 30
+        PORTD,         // 31
+        PORTD,         // 32
+        INVALID_VALUE, // 33
+        INVALID_VALUE, // 34
+};
+
+static int8_t PinToPortPin[PIN_MAX] =
+    {
+        INVALID_VALUE, // 0
+        PIN3,          // 1
+        PIN4,          // 2
+        INVALID_VALUE, // 3
+        INVALID_VALUE, // 4
+        INVALID_VALUE, // 5
+        INVALID_VALUE, // 6
+        PIN6,          // 7
+        PIN7,          // 8
+        PIN5,          // 9
+        PIN6,          // 10
+        PIN7,          // 11
+        PIN0,          // 12
+        PIN1,          // 13
+        PIN2,          // 14
+        PIN3,          // 15
+        PIN4,          // 16
+        PIN5,          // 17
+        INVALID_VALUE, // 18
+        INVALID_VALUE, // 19
+        INVALID_VALUE, // 20
+        INVALID_VALUE, // 21
+        INVALID_VALUE, // 22
+        PIN0,          // 23
+        PIN1,          // 24
+        PIN2,          // 25
+        PIN3,          // 26
+        PIN4,          // 27
+        PIN5,          // 28
+        PIN6,          // 29
+        PIN0,          // 30
+        PIN1,          // 31
+        PIN2,          // 32
+        INVALID_VALUE, // 33
+        INVALID_VALUE, // 34
+};
 
 //*****************************************************************************
 // Function Prototypes
 //*****************************************************************************
 static int8_t GetPortFromPin(uint8_t pin);
-static uint8_t GetPortPinFromPin(uint8_t pin);
+static int8_t GetPortPinFromPin(uint8_t pin);
 static void SetPortsToDefaultConfig();
+static bool PinValid(uint8_t pin);
 
 //*****************************************************************************
 // Function Definitions
 //*****************************************************************************
 void GPIO_Init(GPIO_Port_t *portB_addr, GPIO_Port_t *portC_addr, GPIO_Port_t *portD_addr)
 {
-  // Load default Hardware addresses if needed
-  if (portB_addr == NULL)
-    portB_addr = (GPIO_Port_t *)0x03;
-  if (portC_addr == NULL)
-    portC_addr = (GPIO_Port_t *)0x06;
-  if (portD_addr == NULL)
-    portD_addr = (GPIO_Port_t *)0x09;
-
   GPIO_Ports[PORTB] = portB_addr;
   GPIO_Ports[PORTC] = portC_addr;
   GPIO_Ports[PORTD] = portD_addr;
@@ -61,12 +146,21 @@ void GPIO_Init(GPIO_Port_t *portB_addr, GPIO_Port_t *portC_addr, GPIO_Port_t *po
 
 bool GPIO_GetPin(uint8_t gpioNumber)
 {
-  return true;
+  return (bool)GPIO_Ports[GetPortFromPin(gpioNumber)]->input & (1 << GetPortPinFromPin(gpioNumber));
 }
 
 void GPIO_SetPin(uint8_t gpioNumber, bool state)
 {
-  return;
+  int port = GetPortFromPin(gpioNumber);
+  int portPin = GetPortPinFromPin(gpioNumber);
+
+  if ((port == -1) || (portPin == -1))
+    return;
+
+  if (state)
+    GPIO_Ports[port]->output |= (1 << portPin);
+  else
+    GPIO_Ports[port]->output &= ~(1 << portPin);
 }
 
 void GPIO_ConfigurePin(uint8_t gpioPin, pinConfig_t config)
@@ -96,79 +190,28 @@ void GPIO_ConfigurePin(uint8_t gpioPin, pinConfig_t config)
   }
 }
 
-static uint8_t GetPortPinFromPin(uint8_t pin)
+static int8_t GetPortPinFromPin(uint8_t pin)
 {
-  switch (pin)
-  {
-  case 12:
-  case 23:
-  case 30:
-    return 0;
-  case 13:
-  case 24:
-  case 31:
-    return 1;
-  case 14:
-  case 25:
-  case 32:
-    return 2;
-  case 1:
-  case 15:
-  case 26:
-    return 3;
-  case 2:
-  case 16:
-  case 27:
-    return 4;
-  case 9:
-  case 17:
-  case 28:
-    return 5;
-  case 7:
-  case 10:
-  case 29:
-    return 6;
-  case 8:
-  case 11:
-    return 7;
-  default:
-    return -1;
-  }
+  if (PinValid(pin))
+    return PinToPortPin[pin];
+  else
+    return INVALID_VALUE;
 }
 
 static int8_t GetPortFromPin(uint8_t pin)
 {
-  switch (pin)
-  {
-  case 7:
-  case 8:
-  case 12:
-  case 13:
-  case 14:
-  case 15:
-  case 16:
-  case 17:
-    return PORTB;
-  case 23:
-  case 24:
-  case 25:
-  case 26:
-  case 27:
-  case 28:
-  case 29:
-    return PORTC;
-  case 1:
-  case 2:
-  case 9:
-  case 10:
-  case 11:
-  case 30:
-  case 31:
-  case 32:
-    return PORTD;
-  default:
-    return -1;
-  }
+  if (PinValid(pin))
+    return PinToPort[pin];
+  else
+    return INVALID_VALUE;
+}
+
+static bool PinValid(uint8_t pin)
+{
+  if (pin >= PIN_MAX)
+    return false;
+  else
+    return true;
 }
 
 static void SetPortsToDefaultConfig()
